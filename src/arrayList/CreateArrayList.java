f99ce7d521e3ca923e2b0157ded7ec9cb437ae0c
Mr-DK007 <deepaksahu2052001@gmail.com>
2024-08-15 12:41:20 +0530

    Adding ArrayList concepts

---
+++ src/arrayList/CreateArrayList.java
@@ -0,0 +1,16 @@
+package arrayList;
+
+import java.util.ArrayList;
+
+public class CreateArrayList {
+
+  public static void main(String[] args) {
+
+    // Internal Working: An empty ArrayList object is created. The internal array is initially
+    // empty.
+    ArrayList<String> names = new ArrayList<String>();
+
+    System.out.println("Initial Array List: " + names);
+  }
+
+}
